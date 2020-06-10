e5e4902f4735134e180f96b4be623e0d6eee151c
3s-Phongnd <phong.nd@3si.vn>
2020-06-10 15:47:06 +0700

    [feat] add component

--- contact-application/src/app/app.module.ts
+++ contact-application/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './container/contact-list/contact-list.component';
+import { ContactImageDetailComponent } from './contact-image-detail/contact-image-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ContactListComponent, ContactImageDetailComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
