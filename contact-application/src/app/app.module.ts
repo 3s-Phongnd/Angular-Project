a73c7f9b9611c9a2628cf5a2c2e7023ecd8b495e
3s-Phongnd <phong.nd@3si.vn>
2020-06-12 14:44:46 +0700

    [feat] google map

--- contact-application/src/app/app.module.ts
+++ contact-application/src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './container/contact-list/contact-list.component';
 import { ContactImageDetailComponent } from './contact-image-detail/contact-image-detail.component';
-
+import { NgImageSliderModule } from 'ng-image-slider';
+import { SlideImagesComponent } from './slide-images/slide-images.component';
+import { GoogleMapComponent } from './google-map/google-map.component';
+import { GoogleMapsModule } from '@angular/google-maps';
 @NgModule({
   declarations: [
     AppComponent,
     ContactListComponent,
     ContactImageDetailComponent,
+    SlideImagesComponent,
+    GoogleMapComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgImageSliderModule,
+    GoogleMapsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ contact-application/src/app/google-map/google-map.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-google-map',
+  templateUrl: './google-map.component.html',
+  styleUrls: ['./google-map.component.scss'],
+})
+export class GoogleMapComponent implements OnInit {
+  zoom = 12;
+  center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  };
+  constructor() {}
+
+  ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
+  }
+
+  zoomIn() {
+    if (this.zoom < this.options.maxZoom) {
+      this.zoom++;
+    }
+  }
+
+  zoomOut() {
+    if (this.zoom > this.options.minZoom) {
+      this.zoom--;
+    }
+  }
+}
